0d9e7124d43d2f75e639ce605096827a575321e7
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-03-17 16:09:39 +0900

    API

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_app/screens/login_screen.dart';
 import 'package:flutter_app/widgets/movie_list_page.dart';
 import 'firebase_options.dart';
+import 'models/tmdb_sync_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,6 +46,49 @@ class MyApp extends StatelessWidget {
           return const LoginPage(title: "Next 502 Login");
         },
       ),
+    );
+  }
+}
+
+class MainMapScreen extends StatelessWidget {
+  const MainMapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("부산 빈티지 맵"),
+        actions: [
+          // ✅ TMDB 강제 동기화 버튼
+          IconButton(
+            icon: const Icon(Icons.sync),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => TMDBSyncPage()),
+              );
+            },
+          ),
+
+          // ✅ 영화 리스트 보기
+          IconButton(
+            icon: const Icon(Icons.movie),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const MovieListPage()),
+              );
+            },
+          ),
+
+          // 로그아웃
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => FirebaseAuth.instance.signOut(),
+          )
+        ],
+      ),
+      body: const Center(child: Text("여기에 지도를 띄울 예정입니다!")),
     );
   }
 }
