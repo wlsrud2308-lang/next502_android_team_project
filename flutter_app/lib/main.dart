080071dfdf9c3694dadcd666e29790fbe24b6e43
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-03-19 11:14:12 +0900

    수정완료

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_app/movie_debug_screen.dart';
 import 'package:flutter_app/screens/detail_screen.dart';
 import 'package:flutter_app/screens/home_screen.dart';
 import 'package:flutter_app/screens/login_screen.dart';
